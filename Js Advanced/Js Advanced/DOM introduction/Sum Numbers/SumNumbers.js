f4107fa77cd46984d4b0905696ea081fa0b8c908
Cholev3 <Cholev3@abv.bg>
2024-01-23 21:52:28 +0200

    Update SumNumbers.js

--- Js Advanced/Js Advanced/DOM introduction/Sum Numbers/SumNumbers.js
+++ Js Advanced/Js Advanced/DOM introduction/Sum Numbers/SumNumbers.js
@@ -1,7 +1,6 @@
 function calc() {
     let firstNum = document.getElementById("num1").value
     let secNum = document.getElementById("num2").value
-    let sum = document.getElementById("sum").value
     sum = Number(firstNum) + Number(secNum);
 
     document.getElementById("sum").value = sum;
