cdc603f945c5acfc176c4bc45d656fbe8786828f
Cholev3 <Cholev3@abv.bg>
2024-01-23 22:31:21 +0200

    Update ColorizeTable.js

--- Js Advanced/Js Advanced/DOM introduction/Colorize Table/ColorizeTable.js
+++ Js Advanced/Js Advanced/DOM introduction/Colorize Table/ColorizeTable.js
@@ -1,7 +1,7 @@
 function colorize() {
-    const table = document.getElementsByTagName('tr');
-    for (let i = 1; i < table.length; i += 2) {
-        let item = table[i];
-        item.style.backgroundColor = 'teal';
+    const rows = document.getElementsByTagName('tr');
+    for (let i = 1; i < rows.length; i += 2) {
+        let item = rows[i];
+        item.style.background = 'teal';
     }
 }
