8e9fd0fa9a20ebee4ade0c1504fedae0c079917e
Cholev3 <Cholev3@abv.bg>
2024-02-16 01:30:13 +0200

    Create functionalSum.js

---
+++ Js Advanced/Js Advanced/Advanced Functions - Exercise/functionalSum.js
@@ -0,0 +1,13 @@
+function add(num) {
+    let sum = num
+
+    function calculate(num2) {
+        sum += num2;
+        return calculate;
+    }
+
+    calculate.toString = function () { return sum };
+    return calculate;
+}
+console.log(add(1))
+console.log(add(1)(6)(-3))
